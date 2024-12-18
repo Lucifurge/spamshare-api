82d833501b3a001004358209159eb4bf3fb802ea
Lucifurge <tasogaresaya@gmail.com>
2024-12-18 15:55:29 +0800

    Add files via upload

--- server.js
+++ server.js
@@ -1,12 +1,16 @@
 const express = require("express");
 const puppeteer = require("puppeteer");
 const cors = require("cors");
+const path = require("path");
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
+
+// Serve static files from the "public" directory
+app.use(express.static(path.join(__dirname, "public")));
 
 // Route to handle spamshare request
 app.post("/spamshare", async (req, res) => {
